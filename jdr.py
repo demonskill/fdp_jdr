673b86a71eb38ac508efabb79b18416e8b69c75f
demonskill <guillgatt@gmail.com>
2020-09-03 15:49:24 +0200

    diverses amélioration création de personnage

--- jdr.py
+++ jdr.py
@@ -35,6 +35,131 @@ def multipleroll(b):
     s=s+r
     return s
 
+def creation_perso(mode,tirage,svg) :
+    fdp = svg;
+    perso = [];
+    if mode == "m" : #mode manuel
+        print ("Quel est le nom du personnage n°",l+1,"?");
+        nom = input();
+        print ("Quel est la race du personnage n°",l+1,"?");
+        race = input();
+        print("Quel est le sexe du personnage n°",l+1,"?");
+        sexe = input();
+        sexe = "sexe : " + sexe
+    if mode == "p": #mode procédural (seul le nom est choisi aléatoirement)
+        import os
+        i = 0
+        liste = os.listdir()
+        ftxt=[]
+        while i< len(liste):
+            if "listenom" in liste[i] and".txt" in liste[i]:
+                ftxt.append(liste[i])
+            i= i+1
+        i= 0
+        listrace= []
+        while i<len(ftxt):
+            race = ftxt[i]
+            race = race[9:-5]
+            if race not in listrace:
+                listrace.append(race)
+            i= i+1
+        race =""
+        print("Quel est la race de ton personnage ? (parmi " ,', '.join(listrace),")")
+        race = input()
+        while race not in listrace:
+            print("Veuillez selectionnez une race valide");
+            race = input()   
+        print("le personnage est de quel sexe ? (0 pour neutre, 1 pour homme, 2 pour femme):",);
+        sexe =input()
+        fname = "listenom " + race + sexe +".txt";
+        while fname not in ftxt:
+
+            print("ce sexe n'existe pas pour cette race ou alors n'est pas dans le dossier du programme");
+            sexe = input();
+            fname = "listenom " + race + sexe +".txt";
+        file = open(fname,"r", encoding="utf-8");
+        txt = file.readlines();
+        maxr = len(txt)-1;
+        rnum= random.randint(0, maxr);
+        nom = txt[rnum];
+        nom = nom.rstrip();
+        sexecon = ["N","M","F"]
+        sexe = "sexe : " + sexecon[int(sexe)]
+    if mode == "a": #mode automatique (purement aléatoire)
+        import os
+        i = 0
+        liste = os.listdir()
+        ftxt=[]
+        while i< len(liste):
+            if "listenom" in liste[i] and".txt" in liste[i]:
+                ftxt.append(liste[i])
+            i= i+1
+        i= 0
+        maxnbr= len(ftxt)-1
+        filenbr = random.randint(0, maxnbr)
+        fname = ftxt[filenbr]
+        race = fname[9:-5]
+        sexe = int(fname[-5])
+        file = open(fname,"r", encoding="utf-8");
+        txt = file.readlines();
+        maxr = len(txt)-1;
+        rnum= random.randint(0, maxr);
+        nom = txt[rnum];
+        nom = nom.rstrip()
+        sexecon = ["N","M","F"]
+        sexe = "sexe : " + sexecon[sexe]
+    perso.append(nom);
+    perso.append(race);
+    perso.append(sexe);
+    fdp = fdp.replace("nom",nom);
+    fdp = fdp.replace("race :",race);
+    fdp = fdp.replace("sexe :",sexe);
+    i = 1;
+    b = 0;
+    while i <= c: # tirage des caractéristiques
+        i = i+1;
+        score = int(multipleroll(tirage));
+        perso.append(score);
+        num = str(i-1);
+        stat = "stat " + num;
+        posc = fdp.find(stat);
+        if posc >0 : # ajout du tirage obtenu sur le svg
+            comp = str(score);
+            fdp = fdp[:posc]+ comp + fdp[posc+6:];
+    fnom = "fdp_" + nom + ".svg";
+    file = open(fnom, "w+", encoding="utf-8");
+    file.write(fdp);
+    file.close
+    print (perso);
+    i = 0;
+    ligneperso = "";
+    while b <= c+2: # pour permettre l'export de plusieurs personnage
+        t=str(perso[b])
+        ligneperso = ligneperso + "," + t ;
+        b= b+1;
+    return ligneperso
+
+def multicreation_perso(tirage,svg,listeperso) :
+    print ("Combien de personnages vont être crées ?") #séléction du nombre de personnage
+    while True:
+        try:
+            personbr=int(input())
+            break
+        except ValueError:
+            print("Veuillez saisir un nombre entier")
+    mode=""
+    print("Comment voulez-vous que les noms, races, sexe soit choisi ? (m pour manuel, a pour aléatoire, p pour procédural")
+    mode = input()
+    for l in range(personbr) :
+        ligneperso = creation_perso(mode,tirage,svg)[1:]
+        listeperso.append(ligneperso)
+    print("Voulez-vous tirer de nouveaux personnage ? (y pour dire oui)")
+    rep=input()
+    if rep=="y" :
+        multicreation_perso(tirage,svg,listeperso)
+    return listeperso
+        
+
 file = open("fdpmodele.svg","r",encoding="utf-8") #importation du modèle svg
 svg = file.read()
 file.close
@@ -108,125 +233,7 @@ while True:
     except:
         print("Veuillez donner un lancer de dès valide")
         tirage = input()
-print ("Combien de personnages vont être crées ?") #séléction du nombre de personnage
-while True:
-    try:
-        personbr=int(input())
-        break
-    except ValueError:
-        print("Veuillez saisir un nombre entier")
-l= 0
-listeperso = []
-mode=""
-print("Comment voulez-vous que les noms, races, sexe soit choisi ? (m pour manuel, a pour aléatoire, p pour procédural")
-mode = input()
-while mode != "m" and mode != "a" and mode != "p":
-    print("Veuillez saisir m ou a ou p")
-    mode = input()
-while l < personbr :
-    fdp = svg;
-    perso = [];
-    if mode == "m" : #mode manuel
-        print ("Quel est le nom du personnage n°",l+1,"?");
-        nom = input();
-        print ("Quel est la race du personnage n°",l+1,"?");
-        race = input();
-        print("Quel est le sexe du personnage n°",l+1,"?");
-        sexe = input();
-        sexe = "sexe : " + sexe
-    if mode == "p": #mode procédural (seul le nom est choisi aléatoirement)
-        import os
-        i = 0
-        liste = os.listdir()
-        ftxt=[]
-        while i< len(liste):
-            if "listenom" in liste[i] and".txt" in liste[i]:
-                ftxt.append(liste[i])
-            i= i+1
-        i= 0
-        listrace= []
-        while i<len(ftxt):
-            race = ftxt[i]
-            race = race[9:-5]
-            if race not in listrace:
-                listrace.append(race)
-            i= i+1
-        race =""
-        print("Quel est la race de ton personnage ? (parmi " ,', '.join(listrace),")")
-        race = input()
-        while race not in listrace:
-            print("Veuillez selectionnez une race valide");
-            race = input()   
-        print("le personnage est de quel sexe ? (0 pour neutre, 1 pour homme, 2 pour femme):",);
-        sexe =input()
-        fname = "listenom " + race + sexe +".txt";
-        while fname not in ftxt:
-            print("ce sexe n'existe pas pour cette race ou alors n'est pas dans le dossier du programme");
-            sexe = input();
-            fname = "listenom " + race + sexe +".txt";
-        file = open(fname,"r", encoding="utf-8");
-        txt = file.readlines();
-        maxr = len(txt)-1;
-        rnum= random.randint(0, maxr);
-        nom = txt[rnum];
-        nom = nom.rstrip();
-        sexecon = ["N","M","F"]
-        sexe = "sexe : " + sexecon[int(sexe)]
-    if mode == "a": #mode automatique (purement aléatoire)
-        import os
-        i = 0
-        liste = os.listdir()
-        ftxt=[]
-        while i< len(liste):
-            if "listenom" in liste[i] and".txt" in liste[i]:
-                ftxt.append(liste[i])
-            i= i+1
-        i= 0
-        maxnbr= len(ftxt)-1
-        filenbr = random.randint(0, maxnbr)
-        fname = ftxt[filenbr]
-        race = fname[9:-5]
-        sexe = int(fname[-5])
-        file = open(fname,"r", encoding="utf-8");
-        txt = file.readlines();
-        maxr = len(txt)-1;
-        rnum= random.randint(0, maxr);
-        nom = txt[rnum];
-        nom = nom.rstrip()
-        sexecon = ["N","M","F"]
-        sexe = "sexe : " + sexecon[sexe]
-    perso.append(nom);
-    perso.append(race);
-    perso.append(sexe);
-    fdp = fdp.replace("nom",nom);
-    fdp = fdp.replace("race :",race);
-    fdp = fdp.replace("sexe :",sexe);
-    i = 1;
-    b = 0;
-    while i <= c: # tirage des caractéristiques
-        i = i+1;
-        score = int(multipleroll(tirage));
-        perso.append(score);
-        num = str(i-1);
-        stat = "stat " + num;
-        posc = fdp.find(stat);
-        if posc >0 : # ajout du tirage obtenu sur le svg
-            comp = str(score);
-            fdp = fdp[:posc]+ comp + fdp[posc+6:];
-    fnom = "fdp_" + nom + ".svg";
-    file = open(fnom, "w+", encoding="utf-8");
-    file.write(fdp);
-    file.close
-    print (perso);
-    i = 0;
-    l= l+1;
-    ligneperso = "";
-    while b <= c+2: # pour permettre l'export de plusieurs personnage
-        t=str(perso[b])
-        ligneperso = ligneperso + "," + t ;
-        b= b+1;
-    ligneperso = ligneperso[1:];
-    listeperso.append(ligneperso) 
+listeperso=multicreation_perso(tirage,svg,[])
 print("Voulez-vous exporter en csv (tableur) ? (y pour dire oui)");
 reponse = input();
 if reponse == "y": #export en csv
@@ -234,15 +241,12 @@ if reponse == "y": #export en csv
     with open('jdr.csv', 'w', newline='',encoding="utf8") as csvfile:
         writer = csv.writer(csvfile, delimiter=' ',)
         fieldnames = []
-        while i <= c+2:
+        for i in range(c+3) :
             fieldnames.append (listcarac[i]) ;
-            i = i+1;
         headwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
         headwriter.writeheader()
         personnage =""
-        n = 0
-        while n < personbr:
+        for n in range(len(listeperso)):
             personnage = listeperso[n]
             personnage = personnage.replace('"','')
             writer.writerow([personnage]);
-            n = n+1
